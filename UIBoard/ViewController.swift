fe0d8bb43f4625dd5bea92a219a3b6df7873ede7
Alex Vasilyev <i-am-av@yandex.ru>
2025-08-13 19:06:17 +0300

    Initial Commit

---
+++ UIBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIBoard
+//
+//  Created by Alex Vasilyev on 13.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
